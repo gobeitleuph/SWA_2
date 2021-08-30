5f4d6c44f63572a4afffd201aae159e7ad851301
gobeitleuph <64547252+gobeitleuph@users.noreply.github.com>
2021-08-30 11:39:31 +0200

    Patrick test

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,17 @@
+import Overview.Booking.BookingService;
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("test1");
+
+        BookingService bk1 = new BookingService();
+
+        bk1.createBooking();
+
+        System.out.println();
     }
 
 }
